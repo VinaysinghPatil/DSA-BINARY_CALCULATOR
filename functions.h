d5600c56f21f8ea7d2458b0db4b09dd9b7655d83
Vinaysingh Patil <69345310+VinaysinghPatil@users.noreply.github.com>
2022-08-13 20:22:57 +0530

    Add files via upload

---
+++ functions.h
@@ -0,0 +1,11 @@
+#define SCALE 50
+
+int compareEqual(number a, number b);
+void decimalEqual(number *a, number *b);
+number *add(number *a, number *b);
+number *sub(number *a, number *b);
+number *mult(number *a, number *b);
+number *division(number *a, number *b);
+number *modulus(number *a, number *b);
+number *power(number *a, number *b);
+//functions for sine,cosine,tangent will be developed soon.
